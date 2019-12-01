61e5212d0d0ba2bcd2ca0ff408c3981693d20495
DragonCoder01 <dragon-coder@hotmail.com>
2019-12-01 22:18:55 +0100

    Solution for Day 1

---
+++ d1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+int calcFuel(int weight) {
+	return weight / 3 - 2;
+}
+
+int calcFuelForFuel(int fuel) {
+	int a = calcFuel(fuel);
+	if (a <= 0) return 0;
+	return a + calcFuelForFuel(a);
+}
+
+int main() {
+	std::cout << "Day 1" << std::endl;
+	std::ifstream file("input.txt");
+
+	int a;
+	long b = 0;
+	long c = 0;
+	while (file >> a) {
+		// std::cout << a << "\n";
+		b += calcFuel(a);
+		c += calcFuelForFuel(calcFuel(a));
+	}
+	std::cout << "The result for part 1 is: " << b << "\n";
+	std::cout << "The result for part 2 is: " << c + b << "\n";
+
+	// std::cout << "Test for the recursive function: 100756: " << calcFuelForFuel(100756);
+	return 0;
+}
