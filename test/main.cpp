21eacd592932bb24e7e702045699cf3ead01a7f4
DragonCoder01 <dragon-coder@hotmail.com>
2019-12-07 22:25:00 +0100

    Updated Readme
    And installed Ncurses on my system and build a little "Hello World"
    with it

--- test/main.cpp
+++ test/main.cpp
@@ -2,18 +2,30 @@
 #include "computer/Instructionset.h"
 #include <iostream>
 
-int main() {
-    InstructionSet instruction_set;
-    instruction_set.enable_all();
-    Computer com = Computer(instruction_set.get());
+#include <ncurses.h>
 
-    while (true) {
-        static std::string command;
-        std::cout << "$ (Note: It does nothing right now ;D)";
-        std::getline(std::cin, command);
-        std::cout << command;
-    }
+int main() {
+    // InstructionSet instruction_set;
+    // instruction_set.enable_all();
+    // Computer com = Computer(instruction_set.get());
+
+    // while (true) {
+    //     static std::string command;
+    //     std::cout << "$ (Note: It does nothing right now ;D)";
+    //     std::getline(std::cin, command);
+    //     std::cout << command;
+    //     if (command.find("load")) {
+    //         std::cout << "EYYY; found load :D \n";
+    //     }
+    // }
+
+    initscr();
+    printw("Hello Ncurses!");
+    refresh();
+    getch();
+    endwin();
+    return 0;
 
     
-    com.execute();
+    // com.execute();
 }
